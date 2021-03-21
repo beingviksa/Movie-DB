0b47295fb88a4947f8c70fcf2f6e912adaf42b9b
Vikram Singh <singhvikram143777@gmail.com>
2021-03-21 20:05:38 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,106 +1,71 @@
 import React, { useState, useEffect } from "react";
-import List from "./List";
-import Alert from "./Alert";
-
-const getLocalStorage = () => {
-  let list = localStorage.getItem("list");
-
-  if (list) {
-    return JSON.parse(localStorage.getItem("list"));
-  } else {
-    return [];
-  }
-};
-
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 function App() {
-  const [name, setName] = useState("");
-  const [list, setList] = useState(getLocalStorage());
-  const [isEditing, setIsEditing] = useState(false);
-  const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({
-    show: false,
-    msg: "",
-    type: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!name) {
-      showAlert(true, "please enter value", "danger");
-    } else if (name && isEditing) {
-      setList(
-        list.map((item) => {
-          if (item.id === editID) {
-            return { ...list, title: name };
-          }
-          return item;
-        })
-      );
-      setName("");
-      setEditID(null);
-      setIsEditing(false);
-      showAlert(true, "Item updated", "success");
-    } else {
-      showAlert(true, "Item added", "success");
-      const newItem = { id: new Date().getTime().toString(), title: name };
-      setList([...list, newItem]);
-      setName("");
-    }
-  };
-
-  const showAlert = (show = false, msg = "", type = "") => {
-    setAlert({ show, msg, type });
-  };
-
-  const clearList = () => {
-    showAlert(true, "empty list", "danger");
-    setList([]);
-  };
-
-  const deleteItem = (id) => {
-    const filteredItems = list.filter((item) => item.id !== id);
-    showAlert(true, "item deleted", "danger");
-    setList(filteredItems);
-  };
-
-  const editItem = (id) => {
-    const specificItem = list.find((item) => item.id === id);
-    setIsEditing(true);
-    setEditID(id);
-    setName(specificItem.title);
-  };
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowes] = useState([]);
 
   useEffect(() => {
-    localStorage.setItem("list", JSON.stringify(list));
-  }, [list]);
+    if (loading) return;
+    setFollowes(data[page]);
+  }, [loading, page]);
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    });
+  };
+
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length - 1;
+      }
+      return prevPage;
+    });
+  };
 
   return (
-    <section className="section-center">
-      <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
-        <h3>grocery bud</h3>
-        <div className="form-control">
-          <input
-            type="text"
-            className="grocery"
-            placeholder="e.g. eggs"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <button type="submit" className="submit-btn">
-            {isEditing ? "edit" : "submit"}
-          </button>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading" : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <div className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
         </div>
-      </form>
-      {list.length > 0 && (
-        <div className="grocery-container">
-          <List items={list} deleteItem={deleteItem} editItem={editItem} />
-          <button className="clear-btn" onClick={clearList}>
-            clear items
-          </button>
-        </div>
-      )}
-    </section>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={prevPage}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => setPage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={nextPage}>
+              next
+            </button>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
 
